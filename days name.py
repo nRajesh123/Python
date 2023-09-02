0320d844ee04d69601edd2e639f4a1d5423b6369
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-02 10:56:33 -0700

    Add files via upload

---
+++ days name.py
@@ -0,0 +1,18 @@
+#Write apyhon program to print days names according to the numbers from 1 to 7.
+n=int(input("Enter the number of the day:"))
+if n==1:
+    print("Monday")
+elif n==2:
+    print("Tuesday")
+elif n==3:
+    print("Wednesday")
+elif n==4:
+    print("Thursday")
+elif n==5:
+    print("Friday")
+elif n==6:
+    print("Saturday")
+elif n==7:
+    print("Sunday")
+else:
+    print("Wrong value")
---
+++ expenditure.py
@@ -0,0 +1,8 @@
+#Akash has x money,he spends y money each day in a month having 30 days.
+#if Akash has enough money print Yes else print No.
+x=int(input("Enter x:"))
+y=int(input("Enter y:"))
+if y*30<=x:
+    print("yes")
+else:
+    print("No")
