ca0f4cd071ef4b03c3b6996529b742d05ccac9d0
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-27 21:59:50 +0530

    Add files via upload

---
+++ Biggest of two num....py
@@ -0,0 +1,4 @@
+#python code to average of two numbers
+a,b=map(int,input("enter a and b values:").split())
+avg=(a+b)/2
+print("The average of", a, "and", b,"is",avg)
---
+++ even or not...py
@@ -0,0 +1,9 @@
+a,b=map(int,input("Enter the a and b : ").split())
+ad=a+b
+s=a-b
+m=a*b
+d=a/b
+print("Adition of ",a,"and",b,"is",ad)
+print("Subtraction  of ",a,"and",b,"is",s)
+print("Multiplication of ",a,"and",b,"is",m)
+print("Division  of ",a,"and",b,"is",d)
