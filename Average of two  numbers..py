ddbb79bba271b3a8ea218d4908cb6762be650c9d
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-28 21:33:22 +0530

    Update Average of two  numbers..py

--- Average of two  numbers..py
+++ Average of two  numbers..py
@@ -1,8 +1,6 @@
-#1 to 20 except divisible by 3 and 5
-m,n=map(int,input(" Enter m and n:"))
-for i in range(m,n+1):
-    if i%3==0 and i%5==0:
-        continue
-    print(i,end=" ")
+#python code to average of two numbers
+a,b=map(int,input("enter a and b values:").split())
+avg=(a+b)/2
+print("The average of", a, "and", b,"is",avg)
     
     
