afd5c84cbb8f065744585df245d99e5bd61a9c41
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-28 21:26:25 +0530

    Update Prime or Not.py

--- Prime or Not.py
+++ Prime or Not.py
@@ -4,8 +4,8 @@ for i in range(2,n):
   if n%i==0:
         pr=1
         break
-    if pr==1:
-        print(" Not prime")
-    else:
-        print(" prime")
+if pr==1:
+    print(" Not prime")
+else:
+    print(" prime")
     
