ea3ccd2be51d59e499374edf74267137d9789156
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-28 21:37:39 +0530

    Update Arthmatic Operations..py

--- Arthmatic Operations..py
+++ Arthmatic Operations..py
@@ -1,5 +1,10 @@
-#program to print nos dtw 1& 20 ecept nos divisible by 31
-for i in range(1,21):
-    if i%3==0:
-        continue
-    print(" {}".format(i))
+#pytho program to Arthmatic Operations
+a,b=map(int,input("Enter the a and b : ").split())
+ad=a+b
+s=a-b
+m=a*b
+d=a/b
+print("Adition of ",a,"and",b,"is",ad)
+print("Subtraction  of ",a,"and",b,"is",s)
+print("Multiplication of ",a,"and",b,"is",m)
+print("Division  of ",a,"and",b,"is",d)
