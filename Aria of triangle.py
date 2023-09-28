8cd55ca78f4a2da99137eff7b35c65f334064038
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-28 22:01:08 +0530

    Update Aria of triangle.py

--- Aria of triangle.py
+++ Aria of triangle.py
@@ -1,4 +1,4 @@
 #pyhton code to find aria of a triangle
 b,h=map(int,input("Enter b abd h of the tiangle:").split())
 a=(1/2)*b*h
-print("The aria of the triangle =",a)
+print("The aria of the triangle ={:.2f}".format(a))
