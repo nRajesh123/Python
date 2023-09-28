fca75dd3744fb8aed5e04d6996e3a7beecc93f3c
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-28 22:02:54 +0530

    Update Aria of rectangle.py

--- Aria of rectangle.py
+++ Aria of rectangle.py
@@ -1,4 +1,4 @@
 #pyhton code to find aria of a rectangle
 b,h=map(int,input("Enter b abd h of the rectangle:").split())
 a=b*h
-print("The aria of the rectangle =",a)
+print("The aria of the rectangle ={:.2f}".format(a))
