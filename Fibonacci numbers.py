e299b0e37439fcbf5ddaf1ff033e1a23e5433042
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-09-29 21:25:50 +0530

    Add files via upload

---
+++ Fibonacci numbers.py
@@ -0,0 +1,12 @@
+n=int(input())
+f1,f2=0,1
+counter=0
+if (n==1):
+    print(f1)
+else:
+    while (counter<n):
+        print(f1,end=" ")
+        f3=f1+f2
+        counter+=1
+        f1,f2=f2,f3
+        
