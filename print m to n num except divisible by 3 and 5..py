c80730c796082314ac9f0eabc8335b9de110eb88
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-10-01 23:50:04 +0530

    Update print m to n num except divisible by 3 and 5..py

--- print m to n num except divisible by 3 and 5..py
+++ print m to n num except divisible by 3 and 5..py
@@ -1,6 +1,7 @@
-n=int(input("Enter the n value:"))
-if(n%2==0):
-    print(" The given number is even")
-else:
-    print(" The given number is odd")
-    
+# print m to n num except divisible by 3 and 5
+m,n=map(int,input(" Enter m and n:").split())
+print("The numbrs from {} to {} ecmxcept divisible by 3 and 5 are:".format(m,n))
+for i in range(m,n+1):
+    if i%3==0 or i%5==0:
+        continue
+    print(i,end=" ")
