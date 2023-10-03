bdaf1e66f691292e14892651474e9784152d386c
RR Rajesh N <134297616+nRajesh123@users.noreply.github.com>
2023-10-03 19:39:17 +0530

    Update choosing Random number using random function.py

--- choosing Random number using random function.py
+++ choosing Random number using random function.py
@@ -1,7 +1,5 @@
-# print m to n num except divisible by 3 and 5
+#choosing random num between m and n
+import random
 m,n=map(int,input(" Enter m and n:").split())
-print("The numbrs from {} to {} ecmxcept divisible by 3 and 5 are:".format(m,n))
-for i in range(m,n+1):
-    if i%3==0 or i%5==0:
-        continue
-    print(i,end=" ")
+a=random.randint(m,n)
+print("The selected number is",a)
